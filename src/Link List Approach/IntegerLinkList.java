63c7e5499b7f0bbfdf67c1db2b26c6b10e200f36
Ramin <166108058+Ramin2005@users.noreply.github.com>
2025-11-30 18:03:57 +0330

    Create IntegerLinkList.java

---
+++ src/Link List Approach/IntegerLinkList.java
@@ -0,0 +1,32 @@
+
+public class IntegerLinkList {
+
+    private int value;
+    private IntegerLinkList next;
+
+    public IntegerLinkList() {
+
+    }
+
+    public IntegerLinkList(int value) {
+        this.value = value;
+    }
+
+    public IntegerLinkList(int value, IntegerLinkList next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
+    public void setNextNode(IntegerLinkList newNext) {
+        this.next = newNext;
+    }
+
+    public void setNextNode(int newNext) {
+        this.next = new IntegerLinkList(newNext);
+    }
+
+}
