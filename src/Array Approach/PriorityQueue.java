8942d901d946f56b0f21a27814a96e33bf13112a
Ramin <166108058+Ramin2005@users.noreply.github.com>
2025-12-08 11:16:54 +0330

    Update PriorityQueue.java

--- src/Array Approach/PriorityQueue.java
+++ src/Array Approach/PriorityQueue.java
@@ -1,4 +1,13 @@
 
 public class PriorityQueue {
 
+    private Object[] data;
+    private long[] priority;
+    private int size;
+
+    private int minPriorityIndex = -1;
+    private int minPriority = 2147483646;
+
+    private int maxPriorityIndex = -1;
+    private int maxPriority = -2147483648;
 }
